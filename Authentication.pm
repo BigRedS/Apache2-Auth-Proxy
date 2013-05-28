1d7805905df745fde0124a8f2e20d50096b8d296
Avi Greenbury <ialoneambest@gmail.com>
2013-05-28 14:13:38 +0100

    Added code to deal with caching. Still needs to work with TTLs properly

--- Authentication.pm
+++ Authentication.pm
@@ -11,46 +11,10 @@ use Apache2::RequestRec;
 use Apache2::Const -compile => qw/FLAG/;
 use Apache2::Log;
 use LWP::UserAgent;    # libwww-perl
-
+use Apache::Htpasswd;   # libapache-htpasswd-perl
 use Data::Dumper;
 
 use Apache2::Const -compile => qw(OK DECLINED HTTP_UNAUTHORIZED);
-
-my $conf;
-
-my @directives = (
-	{
-		name => "HTTPAuthProxyURI",
-	},
-	{
-		name => "HTTPAuthProxySSL",
-		args_how => Apache2::Const::FLAG,
-	},
-	{
-		name => "HTTPAuthProxySSLVerifyHostname",
-		args_how => Apache2::Const::FLAG
-
-	}
-);
-
-Apache2::Module::add(__PACKAGE__, \@directives);
-
-sub HTTPAuthProxyURI {
-	my ($self, $parms, $arg) = @_;
-	$conf->{'HTTPAuthProxyURI'} = $arg;
-}
-
-sub HTTPAuthProxySSL {
-	my ($self, $params, $arg) = @_;
-	$self->{'HTTPAuthProxySSL'} = $arg;
-}
-
-sub HTTPAuthProxySSLVerifyHostname {
-	my ($self, $params, $arg) = @_;
-	$self->{'HTPAuthProxySSLVerifyHostname'} = $arg;
-}
-
-
 
 sub handler {
 	my $r = shift;
@@ -61,41 +25,94 @@ sub handler {
 		return Apache2::Const::OK;
 	}else{
 		my $pwlen = length($password);
-		$r->log_reason("Incorrect password for user '".$r->user()." ($password), '", $r->filename);
+		$r->log_reason("Incorrect password for user '".$r->user()."' with password of $pwlen characters, '", $r->filename);
 	}
 	
 	$r->note_basic_auth_failure;
 	return Apache2::Const::HTTP_UNAUTHORIZED;
 }
 
+
 sub checkPassword {
 	my ($username,$password, $r) = @_;
-#	my $url = "https://admin.positive-internet.com";
-#	my $netloc = "admin.positive-internet.com:443";
-#	my $realm = "DB Admin";
-	my $url = "http://truth.posiweb.net/ips";
-	my $netloc = "truth.posiweb.net:80";
-	my $realm = "Here be dragons. And cake.";
-	$r->log_reason("VERIFY_HOSTNAME set to :".$conf->{'HTTPAuthProxySSLVerifyHostname'});
-	$ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = $conf->{'HTTPAuthProxySSLVerifyHostname'};
+	my $url    = $ENV{'AUTHPROXY_URL'};
+	my $netloc = $ENV{'AUTHPROXY_NETLOC'};
+	my $realm  = $ENV{'AUTHPROXY_REALM'};
+	$ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = $ENV{'AUTHPROXY_SSL_VERIFY_HOSTNAME'};
+
+	my $f_cache = $ENV{'AUTHPROXY_CACHE_FILE'} || "/tmp/authproxy.cache";
+	my $htpasswd = Apache::Htpasswd->new({
+		passwdFile => $f_cache,
+	});
+
+	if (-f $f_cache && checkPasswordInCache($htpasswd, $username, $password)){
+		$r->log_error("Auth from cache succeeded for '".$r->user."'");
+		return 1;
+	}else{
+		$r->log_error("Auth from cache failed for '".$r->user."'");
+		if(checkPasswordAgainstRemote($username, $password, $url, $netloc, $realm, $r)){
+			$r->log_error("Auth from remote succeeded for '".$r->user."'");
+			my $ret = updateCache($htpasswd, $username, $password, $r);
+			$r->log_error("Updated Cache : [$ret]");
+			return 1;
+		}else{
+			$r->log_error("Auth failed in cache and from remote for '".$r->user."'");
+			$r->note_basic_auth_failure;
+			return undef;
+		}
+	}
+
+}
+
+sub updateCache {
+	my ($htpasswd,$username,$password, $r) = @_;
+	if($htpasswd->fetchInfo($username) == 0){
+		$r->log_error("User doesn't exist in cache; adding");
+		$htpasswd->htpasswd($username, $password);
+	}else{
+		$r->log_error("Found stale entry in cache, updating");
+		$htpasswd->htpasswd($username, $password, {'overwrite' => 1 });
+	}
+	return 2 unless $htpasswd->writeInfo($username, time());
+}
+
+sub checkPasswordInCache {
+	my ($htpasswd, $username,$password) = @_;
+	my $cache_ttl = $ENV{'AUTHPROXY_CACHE_TTL'} || 86400;
+	if($htpasswd->htCheckPassword($username,$password)){
+		# If the last update (against the remote server) was 
+		# less than a TTL ago we will trust the htpasswd file. 
+		# We make no distinction between the htpasswd (cache) file
+		# having the wrong entry and having no entry.
+		if( my $lastUpdate = $htpasswd->fetchInfo($username) ){
+			if($lastUpdate > (time() - $cache_ttl) ){
+				if($htpasswd->htCheckPassword($username, $password)){
+					return ($username, $password);
+				}
+				return undef;
+			}
+			# If the last update was older than that, we shall simply 
+			# delete the record
+			$r->log_error("Deleting stale entry for '$username' from $lastUpdate")
+			$htpasswd->htDelete($username);
+			return undef;
+		}
+		return undef;
+	}
+
+}
+
+sub checkPasswordAgainstRemote{
+	my ($username, $password, $url, $netloc, $realm, $r) = @_;
 	my $ua = LWP::UserAgent->new();
 	$ua->credentials($netloc, $realm, $username, $password);
 	my $response = $ua->head($url);
 	if($response->is_success){
-		return "1";
+		return ($username,$password);
 	}else{
-		$r->log_reason("LWP said ".$response->status_line, $r->filename);
+		$r->log_error("LWP said: '".$response->status_line."'", $r->filename);
 	}
 	return;
-}
-
-sub checkPasswordInCache {
-	my $cacheFile = "/tmp/.htpasswdcache";
-	
-
-
-
-
 }
 
 1;
